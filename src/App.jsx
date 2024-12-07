f2b700aafe70c6fd39c69c0943ddf7a10d1f82d9
abdelmouiz <a_youb@estin.dz>
2024-12-06 21:41:56 -1100

    the logic of the navbar in themobile device is done

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import NavBar from "./Components/NavBar";
 import HeroSection from "./Components/HeroSection";
 import ShopSection from "./Components/ShopSection";
 import FooterSection from "./Components/FooterSection";
+import { useClick } from "./Store/useClick";
 function App() {
+  const click = useClick((state) => state.click);
   return (
     <div className="app-container">
       <NavBar />
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -17,14 +17,49 @@ function NavBar() {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
+  console.log(click);
   return (
     <div className="navbar-section">
       {resize <= 360 ? (
-        <div className="nav-bar-mobile"></div>
-      ) : resize > 360 && resize < 600 ? (
-        <div className="nav-bar-tablet"></div>
+        <div className="nav-bar-mobile">
+          <div className="logo-nav">logo</div>
+          <div className="icon-nav">
+            <VscThreeBars
+              onClick={() => setClick((prev) => !prev)}
+              className={click ? `white` : `black`}
+            />
+            {click ? (
+              <div className="menu">
+                <div className="menu-items">
+                  <li>
+                    <a href="#">Home</a>
+                  </li>
+                  <li>
+                    <a href="#">About Us</a>
+                  </li>
+                  <li>
+                    <a href="#">Shop</a>
+                  </li>
+                  <li>
+                    <a href="#">Contact Us</a>
+                  </li>
+                </div>
+                <div className="menu-btn">
+                  <button>Sign up</button>
+                  <button>Log In </button>
+                </div>
+              </div>
+            ) : null}
+          </div>
+        </div>
+      ) : resize > 360 && resize <= 600 ? (
+        <div className="nav-bar-tablet">
+          <div className="logo-section-navbar "></div>
+          <div className="link-section"></div>
+          <div className="btn-section-auth"></div>
+        </div>
       ) : (
-        <div className="nav-bar-desktop"></div>
+        <div className="nav-bar-desktop">big screen </div>
       )}
     </div>
   );
